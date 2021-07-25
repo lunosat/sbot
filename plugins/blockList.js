c122630401439328df6cefee4a1ec23f472d55eb
AkirahX <akirah.ofc@gmail.com>
2021-07-25 16:07:55 -0300

    alt

--- plugins/blockList.js
+++ plugins/blockList.js
@@ -1,8 +1,8 @@
 let handler = async (m, { conn }) => {
     let blocked = conn.blocklist.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid)
-    conn.reply(m.chat, `┌ *「 Daftar Terblokir 」*` + `\n` + blocked.map((v, i) => `│ ${i + 1}. @${v.split`@`[0]}`).join`\n` + `\n└────`, m, { contextInfo: { mentionedJid: blocked } })
+    conn.reply(m.chat, `┌ *「 Usuários bloqueados 」*` + `\n` + blocked.map((v, i) => `│ ${i + 1}. @${v.split`@`[0]}`).join`\n` + `\n└────`, m, { contextInfo: { mentionedJid: blocked } })
 }
-handler.help = ['blocklist']
+handler.help = ['listblock']
 handler.tags = ['owner']
 handler.command = /^listbloc?k|bloc?klist|daftarbloc?k$/i
 
