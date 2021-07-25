54562426b0ed8d399df0670470eb35c9a6a69ed2
AkirahX <akirah.ofc@gmail.com>
2021-07-24 23:46:29 -0300

    Add @desc

--- main.js
+++ main.js
@@ -123,10 +123,10 @@ global.reloadHandler = function () {
     conn.off('group-participants-update', conn.onParticipantsUpdate)
     conn.off('CB:action,,call', conn.onCall)
   }
-  conn.welcome = 'Hai, @user!\nSelamat datang di grup @subject\n\n@desc'
-  conn.bye = 'Selamat tinggal @user!'
-  conn.spromote = '@user sekarang admin!'
-  conn.sdemote = '@user sekarang bukan admin!'
+  conn.welcome = 'Olá, @user!\nSeja bem vindo ao grupo @subject\n\n@desc'
+  conn.bye = 'Tchau @user!'
+  conn.spromote = '@user agora é um administrador!'
+  conn.sdemote = '@user não é mais um administrador!'
   conn.handler = handler.handler
   conn.onDelete = handler.delete
   conn.onParticipantsUpdate = handler.participantsUpdate
