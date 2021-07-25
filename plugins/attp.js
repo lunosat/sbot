8ae40a83fb41444b1a7d068c937d0f05d5095b21
AkirahX <akirah.ofc@gmail.com>
2021-07-25 16:06:36 -0300

    Cor

--- plugins/attp.js
+++ plugins/attp.js
@@ -2,9 +2,9 @@ let handler = async (m, { conn, text }) => {
   let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
   conn.sendFile(m.chat, global.API('xteam', '/attp', { file: '', text: teks }), 'attp.webp', '', m, false, { asSticker: true })
 }
-handler.help = ['attp <teks>']
+handler.help = ['ats (texto)']
 handler.tags = ['sticker']
 
-handler.command = /^attp$/i
+handler.command = /^ats$/i
 
 module.exports = handler
