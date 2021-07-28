129cf4836447d18394fa00ca865512240e0bbfc6
AkirahX <akirah.ofc@gmail.com>
2021-07-28 16:54:41 -0300

    NEko

--- plugins/neko.js
+++ plugins/neko.js
@@ -5,8 +5,8 @@ let handler = async(m, { conn }) => {
   let res = await fetch('https://api.waifu.pics/sfw/neko')
   if (!res.ok) throw await res.text()
   let json = await res.json()
-  if (!json.url) throw 'Error!'
-  conn.sendFile(m.chat, json.url, '', 'Nyaa', m)
+  if (!json.url) throw 'Erro!'
+  conn.sendFile(m.chat, json.url, '', 'Nyaaa', m)
 }
 handler.help = ['neko']
 handler.tags = ['internet']
