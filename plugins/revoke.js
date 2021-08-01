f55318b964c65ee62dd14aa99b2bff8d93dfac89
AkirahX <akirah.ofc@gmail.com>
2021-08-01 17:40:49 -0300

    NV

--- plugins/revoke.js
+++ plugins/revoke.js
@@ -2,9 +2,9 @@ let handler = async (m, { conn, args, usedPrefix }) => {
   let res = await conn.revokeInvite(m.chat)
   m.reply('O link do grupo foi redefinido!\n\nLink atual:\nhttps://chat.whatsapp.com/' + res.code)
 }
-handler.help = ['revogar']
+handler.help = ['revogar', 'redefinir']
 handler.tags = ['group']
-handler.command = /^re(definir|new)(invite|link)?$/i
+handler.command = /^re(definir|vogar)(invite|link)?$/i
 handler.group = true
 
 handler.admin = true
