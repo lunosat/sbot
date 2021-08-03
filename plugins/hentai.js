66f341e6666c5b5c5075569013aa197dd3d3e736
AkirahX <akirah.ofc@gmail.com>
2021-08-03 13:46:30 -0300

    Test

--- plugins/hentai.js
+++ plugins/hentai.js
@@ -1,41 +1,7 @@
-async function handler(m, { conn, text, args }) {
-    let chat = global.db.data.chats[m.chat]
-    if (chat.nsfw == true) {
-        m.reply('NSFW = True ')
-      }
-    if (chat.nsfw == false){
-        m.reply('NSFW = False')
-    }
-      return true
+let handler = async (m, {conn, text, usedPrefix}) => {
+  if(!text) throw `*Uso:* ${usedPrefix}hentai (categoria)\n*Exemplo:* ${usedPrefix}hentai anal\n\n*Categorias:*\n_Anal_\nTrap`
+  if(text = 'anal'){
+    conn.reply(m.chat, 'Hentai anal', m)
   }
-  
-  // this is chat-update
-  // handler.all = function (m, chatUpdate) {
-  //  
-  // }
-  
-  // this is executed before normal handler
-  // handler.before = function (m) {
-  //  
-  // }
-  
-  // handler.help = [''].map(v => v + ' <>')
-  // handler.tags = ['']
-  handler.command = ['test']
-  handler.owner = false
-  // handler.mods = false
-  // handler.premium = false
-  // handler.group = false
-  // handler.private = false
-  
-  // handler.admin = false
-  // handler.botAdmin = false
-  
-  // handler.fail = null
-  // handler.exp = 0
-  // handler.level = 1
-  
-  //handler.disabled = true
-  
-  module.exports = handler
-  
+}
+handler.command = /^hentai$/i
