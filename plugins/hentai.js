fd85fcab057644182578ff6ea7e4b016a3faaeb8
AkirahX <akirah.ofc@gmail.com>
2021-08-03 14:01:45 -0300

    Test

--- plugins/hentai.js
+++ plugins/hentai.js
@@ -1,7 +1,28 @@
 let handler = async (m, {conn, text, usedPrefix}) => {
   if(!text) throw `*Uso:* ${usedPrefix}hentai (categoria)\n*Exemplo:* ${usedPrefix}hentai anal\n\n*Categorias:*\n_Anal_\nTrap`
+  categorias = 'anal', 'trap'
+  if(text != categoria) throw 'Categoria inexistente'
   if(text = 'anal'){
-    conn.reply(m.chat, 'Hentai anal', m)
+    try {
+        axios.get(`https://nekos.life/api/v2/img/anal`).then(res => {
+        conn.sendFile(m.chat, res.data.url, 'anal.gif', `Eta `, m)
+        });
+    } catch (err) {
+        console.log(err);
+        throw(err);
+    };
+  return false
+  }
+  if(text = 'trap'){
+    try {
+        axios.get(`https://nekos.life/api/v2/img/trap`).then(res => {
+        conn.sendFile(m.chat, res.data.url, 'anal.gif', `Eta `, m)
+        });
+    } catch (err) {
+        console.log(err);
+        throw(err);
+    };
+  return false
   }
 }
 handler.command = /^hentai$/i
