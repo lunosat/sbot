c2540ba74c593c92774948cab0ef2c06e4f4e18a
AkirahX <akirah.ofc@gmail.com>
2021-08-03 14:45:31 -0300

    Quase

--- plugins/hentai.js
+++ plugins/hentai.js
@@ -1,10 +1,10 @@
 const axios = require('axios')
 let handler = async (m, {conn, text, usedPrefix}) => {
-  if(!text) throw `*Uso:* ${usedPrefix}hentai (categoria)\n*Exemplo:* ${usedPrefix}hentai anal\n\n*Categorias:*\n_anal_\n_trap_\n_boobs_\nblowjob\nkuni\nspank\nclassic`
+  if(!text) throw `*Uso:* ${usedPrefix}hentai (categoria)\n*Exemplo:* ${usedPrefix}hentai anal\n\n*Categorias:*\n_anal_\n_trap_\n_boobs_\n_blowjob_\n_kuni_\n_spank_\n_classic_`
   if(text = 'anal'){
     try {
         axios.get(`https://nekos.life/api/v2/img/anal`).then(res => {
-        conn.sendFile(m.chat, res.data.url, 'anal.gif', `Eta `, m)
+        conn.sendFile(m.chat, res.data.url, 'anal.gif', `Na mosca `, m)
         });
     } catch (err) {
         console.log(err);
@@ -12,10 +12,10 @@ let handler = async (m, {conn, text, usedPrefix}) => {
     };
   return false
   }
-  if(text = 'trap'){
+  else if(text = 'trap'){
     try {
         axios.get(`https://nekos.life/api/v2/img/trap`).then(res => {
-        conn.sendFile(m.chat, res.data.url, 'anal.gif', `Eta `, m)
+        conn.sendFile(m.chat, res.data.url, 'trap.gif', `Trabequin `, m)
         });
     } catch (err) {
         console.log(err);
@@ -23,10 +23,10 @@ let handler = async (m, {conn, text, usedPrefix}) => {
     };
   return false
   }
-  if(text = 'boobs'){
+  else if(text = 'boobs'){
     try {
         axios.get(`https://nekos.life/api/v2/img/boobs`).then(res => {
-        conn.sendFile(m.chat, res.data.url, 'anal.gif', `Eta `, m)
+        conn.sendFile(m.chat, res.data.url, 'boobs.gif', `Deve ser fofin `, m)
         });
     } catch (err) {
         console.log(err);
@@ -34,10 +34,10 @@ let handler = async (m, {conn, text, usedPrefix}) => {
     };
     return false
   }
-  if(text = 'kuni'){
+  else if(text = 'kuni'){
     try {
       axios.get(`https://nekos.life/api/v2/img/kuni`).then(res => {
-      conn.sendFile(m.chat, res.data.url, 'anal.gif', `Eta `, m)
+      conn.sendFile(m.chat, res.data.url, 'kuni.gif', `Saboroso `, m)
       });
   } catch (err) {
       console.log(err);
@@ -45,10 +45,10 @@ let handler = async (m, {conn, text, usedPrefix}) => {
   };
   return false
   }
-  if(text = 'classic'){
+  else if(text = 'classic'){
     try {
         axios.get(`https://nekos.life/api/v2/img/classic`).then(res => {
-        conn.sendFile(m.chat, res.data.url, 'anal.gif', `Eta `, m)
+        conn.sendFile(m.chat, res.data.url, 'classic.gif', `Cultura `, m)
         });
     } catch (err) {
         console.log(err);
@@ -56,10 +56,10 @@ let handler = async (m, {conn, text, usedPrefix}) => {
   };
   return false
   }
-  if(text = 'spank'){
+  else if(text = 'spank'){
     try {
         axios.get(`https://nekos.life/api/v2/img/spank`).then(res => {
-        conn.sendFile(m.chat, res.data.url, 'anal.gif', `Eta `, m)
+        conn.sendFile(m.chat, res.data.url, 'spank.gif', `Ainnn `, m)
         });
     } catch (err) {
         console.log(err);
