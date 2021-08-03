ae9da38d88b9ab17ca991788cff22d6d39afc8bb
AkirahX <akirah.ofc@gmail.com>
2021-08-03 14:22:23 -0300

    Test

--- plugins/hentai.js
+++ plugins/hentai.js
@@ -1,7 +1,15 @@
 const axios = require('axios')
 let handler = async (m, {conn, text, usedPrefix}) => {
   if(!text) throw `*Uso:* ${usedPrefix}hentai (categoria)\n*Exemplo:* ${usedPrefix}hentai anal\n\n*Categorias:*\n_Anal_\nTrap`
-  cat = 'anal', 'trap'
+  var cat = [
+    'anal',
+    'trap',
+    'boobs',
+    'blowjob',
+    'kuni',
+    'spank',
+    'classic',
+  ];
   if(text != cat) throw 'Categoria inexistente'
   if(text = 'anal'){
     try {
