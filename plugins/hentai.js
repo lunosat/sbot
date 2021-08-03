36216160a25ca27fdc7d54fc0bee04690a0523b8
AkirahX <akirah.ofc@gmail.com>
2021-08-03 14:36:36 -0300

    Test

--- plugins/hentai.js
+++ plugins/hentai.js
@@ -1,16 +1,6 @@
 const axios = require('axios')
 let handler = async (m, {conn, text, usedPrefix}) => {
-  if(!text) throw `*Uso:* ${usedPrefix}hentai (categoria)\n*Exemplo:* ${usedPrefix}hentai anal\n\n*Categorias:*\n_Anal_\nTrap`
-  var cat = [
-    'anal',
-    'trap',
-    'boobs',
-    'blowjob',
-    'kuni',
-    'spank',
-    'classic',
-  ];
-  if(text != cat) throw 'Categoria inexistente'
+  if(!text) throw `*Uso:* ${usedPrefix}hentai (categoria)\n*Exemplo:* ${usedPrefix}hentai anal\n\n*Categorias:*\n_anal_\n_trap_\n_boobs_\nblowjob\nkuni\nspank\nclassic`
   if(text = 'anal'){
     try {
         axios.get(`https://nekos.life/api/v2/img/anal`).then(res => {
@@ -32,6 +22,50 @@ let handler = async (m, {conn, text, usedPrefix}) => {
         throw(err);
     };
   return false
+  }
+  if(text = 'boobs'){
+    try {
+        axios.get(`https://nekos.life/api/v2/img/boobs`).then(res => {
+        conn.sendFile(m.chat, res.data.url, 'anal.gif', `Eta `, m)
+        });
+    } catch (err) {
+        console.log(err);
+        throw(err);
+    };
+    return false
+  }
+  if(text = 'kuni'){
+    try {
+      axios.get(`https://nekos.life/api/v2/img/kuni`).then(res => {
+      conn.sendFile(m.chat, res.data.url, 'anal.gif', `Eta `, m)
+      });
+  } catch (err) {
+      console.log(err);
+      throw(err);
+  };
+  return false
+  }
+  if(text = 'classic'){
+    try {
+        axios.get(`https://nekos.life/api/v2/img/classic`).then(res => {
+        conn.sendFile(m.chat, res.data.url, 'anal.gif', `Eta `, m)
+        });
+    } catch (err) {
+        console.log(err);
+        throw(err);
+  };
+  return false
+  }
+  if(text = 'spank'){
+    try {
+        axios.get(`https://nekos.life/api/v2/img/spank`).then(res => {
+        conn.sendFile(m.chat, res.data.url, 'anal.gif', `Eta `, m)
+        });
+    } catch (err) {
+        console.log(err);
+        throw(err);
+    };
+    return false
   }
 }
 handler.command = /^hentai$/i
