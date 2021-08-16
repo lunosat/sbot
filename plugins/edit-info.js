cfa6e33bfc6f0bd8d088acbcdc5aa9648a3a922c
AkirahX <akirah.ofc@hmail.com>
2021-08-16 19:13:32 -0300

    Add eInfo nome

--- plugins/edit-info.js
+++ plugins/edit-info.js
@@ -1,17 +1,22 @@
 let handler = async function (m, { text, usedPrefix }){
     let user = global.db.data.users[m.sender]
-    if(!text) throw 'Coloque sua idade'
-    let age = text
+    let [name, ...age] = text.Spliter(".")
+    if(!age && !name) throw `Formato incorreto!\n\nUtilize o formato *${usedPrefix}einfo Nome.Idade*\n\n*Exemplo:* ${usedPrefix}einfo Ana.21`
+    //let age = text
     if(user.registered == false) throw `Você deve se registrar para alterar sua idade, use ${usedPrefix}registrar`
     age = parseInt(age)
     if(age > 120) throw 'Você está muito velho para isso, altas emoções para seu velho coração.'
     if(age < 5) throw 'Ainda é muito novo para ver o que tem por aqui...'
     user.age = age
-    m.reply(`Sua idade foi alterada com sucesso!
+    user.name = name
+    m.reply(`Suas informações foram alteradas com sucesso!
     
-    _Nova idade:_ *${age}
-    
-    _Verifique em seu *${usedPrefix}perfil*`
+╭─「 Informações 」
+│ Nome: ${name}
+│ Idade: ${age} anos
+╰────
+
+Verifique em seu *!perfil*`
     .trim())
 }
 handler.help = ['idade']
