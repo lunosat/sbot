e6cf560ba5bb4c74487df9c566cfe22d6d956109
AkirahX <akirah.ofc@hmail.com>
2021-08-18 02:29:59 -0300

    btn

--- plugins/btn-test.js
+++ plugins/btn-test.js
@@ -1,22 +1,20 @@
 let { MessageType } = require('@adiwajshing/baileys')
-const { sticker } = require('../lib/sticker')
 let fs = require('fs')
 
-const buttons = [
-{buttonId: '.menu', buttonText: {displayText: 'MENU'}, type: 1},
-{buttonId: '.limit', buttonText: {displayText: 'LIMIT'}, type: 1},
-]
+    const buttons = [
+        {buttonId: '.menu', buttonText: {displayText: 'MENU'}, type: 1},
+        {buttonId: '.profile', buttonText: {displayText: 'PROFILE'}, type: 1},
+    ]
 
-const buttonMessage = {
-contentText: "Ada yang bisa dibantu? \nTekan MENU",
-footerText: 'test doang',
-buttons: buttons,
-headerType: 1
-}
-
-let handler = async (m, { conn }) => {
-conn.sendMessage(m.chat, buttonMessage, MessageType.buttonsMessage, {quoted : m} )
-}
+    const buttonMessage = {
+        contentText: "Hello my love",
+        footerText: 'test',
+        buttons: buttons,
+        headerType: 1
+    }
+    let handler = async (m, { conn }) => {
+    conn.sendMessage(m.chat, buttonMessage, MessageType.buttonsMessage, {quoted : m} )
+s}
 
 
 handler.command = /^btn$/i
