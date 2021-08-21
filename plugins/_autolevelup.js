19cc87d89ee87d15add21e29f9879de8528fe1f3
AkirahX <akirah.ofc@gmail.com>
2021-08-21 18:41:31 -0300

    Bug fix

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -9,16 +9,19 @@ handler.before = m => {
 	while (levelling.canLevelUp(user.level, user.exp, global.multiplier)) user.level++
         
 	if (before !== user.level) {
-		const buttons = [
-			{buttonId: '.perfil' , buttonText: {displayText: 'Perfil'}, type: 1}
-		]
-		const buttonsMessage = {
-			contentText: `_Parabéns, você subiu de nível_!\n\n*${before}* -> *${user.level}*`,
-			footerText: 'Sapphire Network',
-			buttons: buttons,
-			headerType: 1
-		}
-		conn.sendMessage(m.chat, buttonMessage, MessageType.buttonsMessage )
+		let msgUpLevel = `
+_Parabéns, você subiu de nível_!\n\n
+*${before}* -> *${user.level}*`
+    const buttons = [
+      {buttonId: '.perfil', buttonText: {displayText: 'Perfil'}, type: 1}
+    ]
+    const buttonMessage = {
+      contentText: msgUpLevel,
+      footerText: 'Sapphire Network',
+      buttons: buttons,
+      headerType: 1
+    }
+    conn.sendMessage(m.chat, buttonMessage, MessageType.buttonsMessage)
 
 		/*
             m.reply(`
