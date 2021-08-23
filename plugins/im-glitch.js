15c745099bb00bb18ca3e74b19b0b66d56c85bf6
AkirahX <akirah.ofc@gmail.com>
2021-08-23 19:27:45 -0300

    im

--- plugins/im-glitch.js
+++ plugins/im-glitch.js
@@ -2,7 +2,7 @@ let axios = require ('axios')
 let handler = async(m, {conn, text}) => {
     let [txt1, txt2] = text.split("|")
     try{
-        axios.get(`https://sapphire-api.herokuapp.com/api/textmaker?text=akira&text2=pika&theme=glitch&apikey=Alphabot`).then(res => {
+        axios.get(`https://sapphire-api.herokuapp.com/api/textmaker?text=${txt1}&text2=${txt2}&theme=glitch&apikey=Alphabot`).then(res => {
             conn.sendFile(m.chat, res.data.result.url, 'glitch.jpg', 'By Oficial Sapphire API')
         })
     } catch (err) {
