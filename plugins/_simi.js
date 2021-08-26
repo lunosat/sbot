6554e82279cee20837fa6c790827519ac8b3ac0d
AkirahX <akirah.ofc@gmail.com>
2021-08-26 11:16:22 -0300

    Simi AT

--- plugins/_simi.js
+++ plugins/_simi.js
@@ -3,7 +3,11 @@ let { MessageType}  = require('@adiwajshing/baileys')
 let handler = m => m
 handler.before = async function (m) {
     chat = global.db.data.chats[m.chat]
-    if(chat.simi === false) return
+    //if(chat.simi === false) return
+    if (!m.quoted) return
+    let {fromMe, id, isBaileys } = m.quoted
+    if (!fromMe) return
+    if (!isBaileys) return
     let say = m.text
     try{
         axios.get(`https://api.simsimi.net/v1/?text=${encodeURIComponent(say)}&lang=pt`).then(res => {
