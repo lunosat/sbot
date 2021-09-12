e139f3d07a1fecb868dbb7c903ebb7b017c688b8
AkirahX <akirah.ofc@gmail.com>
2021-09-12 16:51:02 -0300

    Fix

--- plugins/_fchat.js
+++ plugins/_fchat.js
@@ -6,7 +6,7 @@ handler.all = async function (m) {
     if (m.chat.endsWith('broadcast')) return
     if (m.fromMe) return
     if (m.isGroup) return
-    if (db.data.settings.groupOnly) return
+    //if (db.data.settings.groupOnly) return
     let user = global.db.data.users[m.sender]
     if (new Date - user.pc < 86400000) return // setiap 24 jam sekali
     await this.sendButton(m.chat, `
