e1cc9fb1ab56fc3a9b1e32762e979f1adabcb334
AkirahX <akirah.ofc@gmail.com>
2021-09-21 22:53:32 -0300

    pes

--- plugins/pesquisa.js
+++ plugins/pesquisa.js
@@ -8,7 +8,9 @@ let handler = async(m, { conn, text }) => {
     for (let jid of global.owner.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid && v != '5511973584242@s.whatsapp.net'))
     m.reply(laporan, jid)
     m.reply(laporan, m.sender) 
-    m.reply('_Muito obrigado por sua reposta!_\n\n._+1000 XP_')
+    m.reply('_Muito obrigado por sua reposta!_\n\n_+1000 XP_')
+    user.pesquisa = true
+    user.exp += 1000
 }
 
 handler.command = /^(r)$/i
