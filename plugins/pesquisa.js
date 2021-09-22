e5d6aaa01ad559c973bda09923b03ee582c9f009
AkirahX <akirah.ofc@gmail.com>
2021-09-21 22:57:18 -0300

    pes

--- plugins/pesquisa.js
+++ plugins/pesquisa.js
@@ -1,16 +1,12 @@
 
 let handler = async(m, { conn, text }) => {
-    let user = global.db.data.users[m.sender]
-    if(user.pesquisa === true) throw 'Você já respondeu a última pesquisa, Nify Tech agradece.'
     if (!text) throw '_Por favor escreva sua reposta._'
     if (text.length > 500) throw 'Seja mais breve, utilize no máximo 500 caracteres.'
     const laporan = `*「 Pesquisa 」*\nNúmero : wa.me/${m.sender.split`@`[0]}\nResposta: ${text}`
     for (let jid of global.owner.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid && v != '5511973584242@s.whatsapp.net'))
     m.reply(laporan, jid)
     m.reply(laporan, m.sender) 
-    m.reply('_Muito obrigado por sua reposta!_\n\n_+1000 XP_')
-    user.pesquisa = true
-    user.exp += 1000
+    m.reply('_Muito obrigado por sua reposta!_')
 }
 
 handler.command = /^(r)$/i
