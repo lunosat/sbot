d6567f1db7774d8cea68a554c1cc88bf1828e5d2
AkirahX <akirah.ofc@gmail.com>
2021-09-27 01:20:57 -0300

    Sticker FIX

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -1,42 +1,67 @@
 const { MessageType } = require('@adiwajshing/baileys')
 const { sticker } = require('../lib/sticker')
+const WSF = require('wa-sticker-formatter')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
   let stiker = false
+  let wsf = false
   try {
     let q = m.quoted ? m.quoted : m
     let mime = (q.msg || q).mimetype || ''
-    if (/image/.test(mime)) {
+    if (/webp/.test(mime)) {
       let img = await q.download()
-      if (!img) throw `Envie na legenda ou marque a imagem com o comando *${usedPrefix + command}*`
-      stiker = await sticker(img, false, global.packname, global.author)
+      if (!img) throw `Marque o sticker com ${usedPrefix + command}`
+      wsf = new WSF.Sticker(img, {
+        pack: global.packname,
+        author: global.author,
+        crop: false,
+      })
+    } else if (/image/.test(mime)) {
+      let img = await q.download()
+      if (!img) throw `Marque a imagem com ${usedPrefix + command}`
+      wsf = new WSF.Sticker(img, {
+        pack: global.packname,
+        author: global.author,
+        crop: false,
+      })
     } else if (/video/.test(mime)) {
-      if ((q.msg || q).seconds > 11) return m.reply('Vídeos/Gifs devem ter no máximo 10 segundos!')
+      if ((q.msg || q).seconds > 11) throw 'O vídeo/Gif deve ter no máximo 10 segundos.'
       let img = await q.download()
-      if (!img) throw `Envie na legenda ou marque o vídeo com o comando *${usedPrefix + command}*`
-      stiker = await sticker(img, false, global.packname, global.author)
-    } else if (/webp/.test(mime)) {
-      let img = await q.download()
-      if (!img) throw `Envie na legenda ou marque o sticker com o comando *${usedPrefix + command}*`
-      stiker = await sticker(img, false, global.packname, global.author)
+      if (!img) throw `Marque o vídeo com ${usedPrefix + command}`
+      wsf = new WSF.Sticker(img, {
+        pack: global.packname,
+        author: global.author,
+        crop: true,
+      })
     } else if (args[0]) {
       if (isUrl(args[0])) stiker = await sticker(false, args[0], global.packname, global.author)
-      else return m.reply('URL inválida!')
+      else throw 'URL inválida'
     }
-  } finally {
-    if (stiker) conn.sendMessage(m.chat, stiker, MessageType.sticker, {
+  } catch (e) {
+    throw e
+  }
+  finally {
+    if (wsf) {
+      await wsf.build()
+      const sticBuffer = await wsf.get()
+      if (sticBuffer) await conn.sendMessage(m.chat, sticBuffer, MessageType.sticker, {
+        quoted: m,
+        mimetype: 'image/webp'
+      })
+    }
+    if (stiker) await conn.sendMessage(m.chat, stiker, MessageType.sticker, {
       quoted: m
     })
-    else throw 'Falha na conversão'
+
   }
 }
 handler.help = ['f (legenda|marque)', 'f (url)', 'fgif (legenda|marque)', 'fgif (url)']
 handler.tags = ['sticker']
 handler.command = /^f(igu?ker)?(gif)?(wm)?$/i
 
-handler.group = true
-
 module.exports = handler
 
 const isUrl = (text) => {
   return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png)/, 'gi'))
 }
+
+
