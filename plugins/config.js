eb8d3883787fcd11e77569dcd292235ed341e01e
AkirahX <akirah.ofc@gmail.com>
2021-10-22 15:40:30 -0300

    config

--- plugins/config.js
+++ plugins/config.js
@@ -3,10 +3,10 @@ let handler = async (m, { conn }) => {
     chat = global.db.data.chats[m.chat]
     const rows = [
         {title: 'Modo NSFW', description: 'Ative ou desative o modo NSFW', rowId: '.nsfw'},
-        {title: 'Simi', description: 'Ative ou desative o SimSimi', rowid: '.simi'},
-        {title: 'Simi', description: 'Ative ou desative o Voice-SimSimi', rowid: '.vsimi'},
-        {title: 'Ant-Pornografia', description: 'Ative ou desative o ant-porn', rowid: '.enable antiporn'},
-        {title: 'Ant-Flood', description: 'Ative ou desative o anti-flood', rowid: '.enable antiflood'},
+        {title: 'Simi', description: 'Ative ou desative o SimSimi', rowId: '.simi'},
+        {title: 'Simi', description: 'Ative ou desative o Voice-SimSimi', rowId: '.vsimi'},
+        {title: 'Ant-Pornografia', description: 'Ative ou desative o ant-porn', rowId: '.enable antiporn'},
+        {title: 'Ant-Flood', description: 'Ative ou desative o anti-flood', rowId: '.enable antiflood'},
 
     ]
     const sections = [{title: 'Configurações', rows: rows}]
