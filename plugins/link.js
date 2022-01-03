00c2eaea8e402fc4a3dd3a58542a08fb06cd7671
AkirahX <82380880+AkirahX@users.noreply.github.com>
2022-01-03 18:26:02 -0300

    H

--- plugins/link.js
+++ plugins/link.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, args }) => {
   let group = m.chat
-  if (/^[0-9]{5,16}-[0-9]+@g\.us$/.test(args[0])) group = args[0]
-  if (!/^[0-9]{5,16}-[0-9]+@g\.us$/.test(group)) throw 'Utilizável somente em grupos'
+  //if (/^[0-9]{5,16}-[0-9]+@g\.us$/.test(args[0])) group = args[0]
+  //if (!/^[0-9]{5,16}-[0-9]+@g\.us$/.test(group)) throw 'Utilizável somente em grupos'
   let groupMetadata = await conn.groupMetadata(group)
   if (!groupMetadata) throw 'groupMetadata is undefined :\\'
   if (!'participants' in groupMetadata) throw 'participants is not defined :('
