91bc60acfa6d8d05537314c12c27c8e5b3be2ea0
AkirahX <82380880+AkirahX@users.noreply.github.com>
2022-01-10 13:14:19 -0300

    Update beta.js

--- plugins/beta.js
+++ plugins/beta.js
@@ -1,5 +1,5 @@
 async function handler(m, { conn, text, args }) {
-    m.reply('Olá, seja bem vindo ao nosso programa de beta testers, durante um periodo você testará nosso novo sistema e receberá como recompensa uma filiação Premium.\n\nVocê deverá testar e enviar seu feedback de cada novo recurso que for adicionado.\n\nEntre aqui: https://chat.whatsapp.com/CIJ40MZBdVL2ovDHVf4tux')
+    m.reply('Olá, seja bem vindo ao nosso programa de beta testers, durante um periodo você testará nosso novo sistema e receberá como recompensa algumas vantagens de acordo com seu nível de ajuda.\n\nVocê deverá testar e enviar seu feedback de cada novo recurso que for adicionado ou que já foi implementado.\n\nEntre aqui: https://chat.whatsapp.com/J3y1RrM4Yx400VXbiinXxy')
   }
   
   // this is chat-update
