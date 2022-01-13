fbf3894f9e45ce59efed8f5433329e7fcc92ab49
AkirahX <akirah.ofc@gmail.com>
2022-01-12 22:03:52 -0300

    db

---
+++ plugins/getdb.js
@@ -0,0 +1,33 @@
+async function handler(m, { conn, text, args }) {
+    conn.sendFile(m.chat, '../database.json', 'database.json', '', m)
+  }
+  
+  // this is chat-update
+  // handler.all = function (m, chatUpdate) {
+  //  
+  // }
+  
+  // this is executed before normal handler
+  // handler.before = function (m) {
+  //  
+  // }
+  
+  // handler.help = [''].map(v => v + ' <>')
+  // handler.tags = ['']
+  handler.command = ['getdb']
+  handler.owner = false
+  // handler.mods = false
+  // handler.premium = false
+  // handler.group = false
+  // handler.private = false
+  
+  // handler.admin = false
+  // handler.botAdmin = false
+  
+  // handler.fail = null
+  // handler.exp = 0
+  // handler.level = 1
+  
+  
+  module.exports = handler
+  
